7d5d985ad4128f4d554fa9e3c24e863cb694b731
shatilov-makar <62944516+shatilov-makar@users.noreply.github.com>
2020-08-21 18:24:03 +0500

    Did async factorial

--- beginner/Program.cs
+++ beginner/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace beginner
 {
@@ -10,9 +11,23 @@ namespace beginner
     {
         static void Main(string[] args)
         {
-            var sd
             Console.WriteLine("Hello world new branch in terminal!");
+            WriteFactorialAsync(6);
+            WriteFactorialAsync(2);
+            WriteFactorialAsync(9);
             Console.ReadKey();
+        }
+
+        static async void WriteFactorialAsync(int num)
+        {
+            var res = await Task.Run(() =>
+            {
+                var r = 1;
+                for (var i = 1; i <= num; i++)
+                    r *= i;
+                return r;
+            });
+            Console.WriteLine($"{num}! = {res}");
         }
     }
 }
